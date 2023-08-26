3b4572006ce7668547ecf5a927b3fcc4c57da1f8
noracato <ncschinkel@gmail.com>
2023-08-26 12:46:45 +0200

    Minor style updates

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // static targets = ['points', 'add', 'subtract', 'submit'];
-
   connect() {
-    // this.addTarget.addEventListener('click', this.addPoint.bind(this));
-    // this.subtractTarget.addEventListener('click', this.subtractPoint.bind(this));
-    // this.submitTarget.addEventListener('click', this.submitPoints.bind(this));
+    this.element.parentElement.style.height = window.innerHeight + "px"
   }
-
-
 }
